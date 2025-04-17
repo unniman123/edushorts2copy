703352bfb0eadbef5ed11a98eaf291f89ef74787
gokuk <gokulravindran08@gmail.com>
2025-04-18 01:09:57 +0530

    solved the building issue in the apk

--- app.config.js
+++ app.config.js
@@ -19,12 +19,9 @@ module.exports = {
   ],
   ios: {
     supportsTablet: true,
-    bundleIdentifier: 'com.ajilkojilgokulravi.unniman',
-    googleServicesFile: './ios/edushorts/GoogleService-Info.plist',
-    associatedDomains: [
-      'applinks:lh1wg.app.link',
-      'applinks:lh1wg-alternate.app.link'
-    ]
+    bundleIdentifier: 'com.ajilkojilgokulravi.unniman', // Updated bundle ID
+    googleServicesFile: './ios/edushorts/GoogleService-Info.plist', // Added GoogleService-Info.plist path
+    associatedDomains: ['applinks:edushorts.app'] // Keep associatedDomains for now
   },
   android: {
     adaptiveIcon: {
@@ -40,11 +37,8 @@ module.exports = {
         data: [
           {
             scheme: "https",
-            host: "lh1wg.app.link"
-          },
-          {
-            scheme: "https",
-            host: "lh1wg-alternate.app.link"
+            host: "edushortlinks.netlify.app",
+            pathPattern: "/article/*"
           }
         ],
         category: ["BROWSABLE", "DEFAULT"]
@@ -54,7 +48,8 @@ module.exports = {
         data: [
           {
             scheme: "edushort",
-            host: "open"
+            host: "article",
+            pathPattern: "/*"
           }
         ],
         category: ["BROWSABLE", "DEFAULT"]
@@ -67,16 +62,13 @@ module.exports = {
   extra: {
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
     supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-    branchKey: "key_live_mtk16153Ngoe3o4XBsd8iehnFDichSM4",
-    branchKeyTest: "key_test_msd11792Nogd5j81wCe6hnbpstfgbSQG",
     eas: {
       projectId: "cfa91622-46a9-49aa-86c3-177c0a05d850"
     }
   },
   plugins: [
-    '@react-native-google-signin/google-signin',
+    '@react-native-google-signin/google-signin', // Added Google Sign-In plugin
     'expo-secure-store',
-    'react-native-branch',
     [
       'expo-build-properties',
       {
